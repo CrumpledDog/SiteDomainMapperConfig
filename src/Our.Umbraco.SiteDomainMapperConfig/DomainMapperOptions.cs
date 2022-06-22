e6a03a9a6555dd42cb858c3c0112cd612d3e24b1
Jeavon Leopold <jeavon@crumpled-dog.com>
2022-06-22 12:38:25 +0100

    Init projects

---
+++ src/Our.Umbraco.SiteDomainMapperConfig/DomainMapperOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Our.Umbraco.SiteDomainMapperConfig
+{
+    public class DomainMapperOptions
+	{
+		public List<Site> Sites { get; set; }
+	}
+
+	public class Site
+	{
+		public string SiteName { get; set; }
+		public string[] Domains { get; set; }
+	}
+}
---
+++ src/Our.Umbraco.SiteDomainMapperConfig/SiteDomainMapperComponent.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Routing;
+using Microsoft.Extensions.Logging;
+
+namespace Our.Umbraco.SiteDomainMapperConfig
+{
+	public class SiteDomainMapperComponent : IComponent
+	{
+        private readonly SiteDomainMapper? _siteDomainMapper;
+		private readonly DomainMapperOptions _domainMapperOptions;
+		private readonly ILogger<SiteDomainMapperComponent> _logger;
+
+		public SiteDomainMapperComponent(ISiteDomainMapper siteDomainMapper, IOptions<DomainMapperOptions> domainMapperOptions, ILogger<SiteDomainMapperComponent> logger)
+		{
+			_domainMapperOptions = domainMapperOptions.Value;
+			_logger = logger;
+
+			// SiteDomainMapper can be overwritten, so ensure it's the default one which contains the AddSite
+			if (siteDomainMapper is SiteDomainMapper concreteSiteDomainMapper)
+			{
+				_siteDomainMapper = concreteSiteDomainMapper;
+			}
+		}
+		/// <inheritdoc/>
+		public void Initialize()
+		{
+			if (_domainMapperOptions.Sites != null)
+			{
+				foreach (var site in _domainMapperOptions.Sites)
+				{
+					_siteDomainMapper?.AddSite(site.SiteName, site.Domains);
+					_logger.LogInformation("SiteDomainMapper added site {SiteName} with {Domains} domains", site.SiteName, string.Join(", ", site.Domains));
+				}
+			}
+		}
+
+		/// <inheritdoc/>
+		public void Terminate()
+		{ }
+	}
+
+	public class SiteDomainMapperComponentComposer : ComponentComposer<SiteDomainMapperComponent>
+	{ }
+}
